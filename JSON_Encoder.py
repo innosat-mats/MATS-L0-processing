0261b24be2ffae28b2c4831f6b71775009db3032
FKann <franz.kanngiesser@runbox.com>
2019-01-10 09:31:18 +0100

    add missing file

---
+++ JSON_Encoder.py
@@ -0,0 +1,13 @@
+import numpy
+import json
+
+class JSON_Encoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, numpy.integer):
+            return int(obj)
+        elif isinstance(obj, numpy.floating):
+            return float(obj)
+        elif isinstance(obj, numpy.ndarray):
+            return obj.tolist()
+        else:
+            return super(JSON_Encoder, self).default(obj)
