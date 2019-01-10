850410ebe2ceb46e3ebdc683178d83e270a002fc
FKann <franz.kanngiesser@runbox.com>
2019-01-10 09:28:12 +0100

    moved CCD data merging to read_racdirectory.py
    This allows to read in image data split over more than one .rac-file

--- read_racdirectory.py
+++ read_racdirectory.py
@@ -18,19 +18,70 @@ from os import listdir
 from read_racfile import read_racfile
 import json
 from JSON_Encoder import JSON_Encoder
+import binascii
 
 #import json
 
 AllDataSorted = [] #List of dicts. Each entry is a packet
-CCD_image_data = [] #List of dicts. Each entry is a CCD image
+CCD_image_data = {} #List of dicts. Each entry is a CCD image
 
 def read_racdirectory(in_directory,out_directory):
     allFiles = listdir(in_directory)
     for i in range(len(allFiles)):
        print(str('Reading file ' + in_directory + '/' + allFiles[i]))
-       tmp1,tmp2 = read_racfile(in_directory + '/' + allFiles[i])
+       #tmp1,tmp2 = read_racfile(in_directory + '/' + allFiles[i])
+       tmp1 = read_racfile(in_directory + '/' + allFiles[i])
        AllDataSorted.extend(tmp1)
-       CCD_image_data.extend(tmp2)
+       
+    #Merge CCD data from several files
+    print(str('Joining images'))
+    CCD_image_data['data channel 1'] = []
+    CCD_image_data['data channel 2'] = []
+    CCD_image_data['data channel 3'] = []
+    
+    
+    for j in range(0,3):
+        print('CCD data channel '+str(j+1))
+        #loop over channels
+    
+        n = -1
+        for x in range(0,len(AllDataSorted)):
+            if AllDataSorted[x]['Source_data']['SID_mnemonic'] == ['CCD data channel '+str(j+1)]:
+                if AllDataSorted[x]['SPH_grouping_flags'] == '01' or AllDataSorted[x]['SPH_grouping_flags'] == '11':
+                    n = n+1 #start new image
+                    #print 'CCD data start'
+                    CCD_image_data['data channel '+str(j+1)].append({}) #start new image
+                    CCD_image_data['data channel '+str(j+1)][n]['data'] = [] 
+                    CCD_image_data['data channel '+str(j+1)][n]['start'] = x 
+                    CCD_image_data['data channel '+str(j+1)][n]['cont'] = [] 
+                    CCD_image_data['data channel '+str(j+1)][n]['stop'] = []
+                    #Add data to image
+                    CCD_image_data['data channel '+str(j+1)][n]['data'].append(AllDataSorted[x]['Source_data']['IMG'])
+                elif AllDataSorted[x]['SPH_grouping_flags'] == '10':
+                    #print 'CCD data stop'
+                    CCD_image_data['data channel '+str(j+1)][n]['stop'] = x 
+                    CCD_image_data['data channel '+str(j+1)][n]['data'].append(AllDataSorted[x]['Source_data']['IMG'])
+                elif AllDataSorted[x]['SPH_grouping_flags'] == '00':
+                    if not CCD_image_data:
+                        print('Warning: current .rac file started with continued CCD data')
+                    CCD_image_data['data channel '+str(j+1)][n]['cont'].append(x) 
+                    CCD_image_data['data channel '+str(j+1)][n]['data'].append(AllDataSorted[x]['Source_data']['IMG'])
+            
+        for x in range(0,len(CCD_image_data['data channel '+str(j+1)])):
+            #get-function allows to check if keywords are existing or not
+            if CCD_image_data['data channel '+str(j+1)][x].get('start')!=None and CCD_image_data['data channel '+str(j+1)][x].get('stop')!=None:
+                if CCD_image_data['data channel '+str(j+1)][x]['start'] and CCD_image_data['data channel '+str(j+1)][x]['stop']:
+                    a = "".join(CCD_image_data['data channel '+str(j+1)][x]['data'])
+                    CCD_image_data['data channel '+str(j+1)][x]['image'] = binascii.unhexlify(a)
+                    CCD_image_data['data channel '+str(j+1)][x]['error'] = 0
+                else:
+                    print('Warning: start or stop does not exist for image: ' + str(x)+', channel '+str(j+1))
+                    CCD_image_data['data channel '+str(j+1)][x]['error'] = 1
+            else:
+                print('Warning: start or stop key does not exist for image: ' + str(x)+', channel '+str(j+1))
+                CCD_image_data['data channel '+str(j+1)][x]['error'] = 1
+       
+     #end channel loop  
 
     filename = out_directory + '/JSON/packets.json'
     with open(filename, 'w') as outfile:
@@ -38,17 +89,18 @@ def read_racdirectory(in_directory,out_directory):
         json.dump(AllDataSorted, outfile, sort_keys = True, indent = 4,
                ensure_ascii = False,cls=JSON_Encoder)
 
-    for i in range(len(CCD_image_data)):
+    for j in range(0,3):#loop over channels again
+        for i in range(len(CCD_image_data['data channel '+str(j+1)])):
     #    #Write images out as jpeg images (for conversion in matlab)
-        CCD_image_data[i]['filename'] = ''
-        if (CCD_image_data[i]['error'] == 0):
-            filename = out_directory + '/IMAGES/test' + str(i) + '.jpg'
-            print(str('Writing file ' + filename))
-            CCD_image_data[i]['filename'] = filename
-            with open(filename,'w') as f:
-                f.write(CCD_image_data[i]['image'])
+            CCD_image_data['data channel '+str(j+1)][i]['filename'] = ''
+            if (CCD_image_data['data channel '+str(j+1)][i].get('error') == 0):
+                filename = out_directory + '/IMAGES/test_channel'+str(j+1)+'_'+ str(i) + '.jpg'
+                print(str('Writing file ' + filename))
+                CCD_image_data['data channel '+str(j+1)][i]['filename'] = filename
+                with open(filename,'w') as f:
+                    f.write(CCD_image_data['data channel '+str(j+1)][i]['image'])
                 
-
+#end loop over channels
         
     filename = out_directory + '/JSON/images.json'
     with open(filename, 'w') as outfile:
--- read_racfile.py
+++ read_racfile.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 
 import numpy
 from read_packet import read_packet
-import binascii
+#import binascii
 
 ##########################################
 #This function takes in MATS payload data and reads one .rac file and extracts it. 
@@ -19,46 +19,12 @@ def read_racfile(filename):
     AllData = []
     while pointer < data.size: #loop over entire file
         [p,pointer] = read_packet(data,pointer) #read package starting at file pointer p
-        AllData.append(p) #append data from package
+        if bool(p) == True: 
+            AllData.append(p) #append data from package
     
     #AllDataSorted is a list with one entry for each package
     AllDataSorted = sorted(AllData, key = lambda user: user['SPH_source_sequence_count']) #Sort based on sequence counts
-    
-    
-    
-    #Merge CCD data from several packets
-    print(str('Joining images'))
-    CCD_image_data = []
-    n = -1
-    for x in range(0,len(AllDataSorted)):
-        if AllDataSorted[x]['Source_data']['SID_mnemonic'] == ['CCD data channel 1']:
-            if AllDataSorted[x]['SPH_grouping_flags'] == '01':
-                n = n+1 #start new image
-                #print 'CCD data start'
-                CCD_image_data.append({}) #start new image
-                CCD_image_data[n]['data'] = [] 
-                CCD_image_data[n]['start'] = x 
-                CCD_image_data[n]['cont'] = [] 
-                CCD_image_data[n]['stop'] = []
-                #Add data to image
-                CCD_image_data[n]['data'].append(AllDataSorted[x]['Source_data']['CCD_image_data'])
-            elif AllDataSorted[x]['SPH_grouping_flags'] == '10':
-                #print 'CCD data stop'
-                CCD_image_data[n]['stop'] = x 
-                CCD_image_data[n]['data'].append(AllDataSorted[x]['Source_data']['CCD_image_data'])
-            elif AllDataSorted[x]['SPH_grouping_flags'] == '00':
-                CCD_image_data[n]['cont'].append(x) 
-                CCD_image_data[n]['data'].append(AllDataSorted[x]['Source_data']['CCD_image_data'])
-                
-    for x in range(0,len(CCD_image_data)):
-        if CCD_image_data[x]['start'] and CCD_image_data[x]['stop']:
-            a = "".join(CCD_image_data[x]['data'])
-            CCD_image_data[x]['image'] = binascii.unhexlify(a)
-            CCD_image_data[x]['error'] = 0
-        else:
-            print('Warning: start or stop does not exist for image: ' + str(x))
-            CCD_image_data[x]['error'] = 1
-                          
-    return AllDataSorted, CCD_image_data
+                             
+    return AllDataSorted#, CCD_image_data
     
 
