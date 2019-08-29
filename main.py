147d3a81f2f7720eb8c8ca085fa4e1cbe700dedc
Ole Martin Christensen <ole.martin.christensen@qamcom.se>
2019-08-29 17:43:23 +0200

    changed main to read from rac directory

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ from read_racdirectory import read_racdirectory
 date = "2017-01-01"
 #in_directory = "./FTP/TM/1/100/" + date
 #out_directory = "./L0_data/" + date
-in_directory = "./EM/"
-out_directory = "./EM_out/" + date
+in_directory = "rac-files"
+out_directory = "rac-out"
 a, b = read_racdirectory(in_directory,out_directory)
